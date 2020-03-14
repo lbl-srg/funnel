69637cd273b75e533bddb7c2c7752f879f2f7bea
AntoineGautier <agautier@lbl.gov>
2020-03-13 22:52:02 -0700

    Include manifest

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     install_requires=['six>=1.11'],
     packages=['pyfunnel'],
     package_data={
-        'pyfunnel': ['templates/*', lib_data],
+        'pyfunnel': [lib_data],
     },
     include_package_data=True,
     classifiers=[
