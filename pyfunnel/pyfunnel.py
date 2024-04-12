43df27117db41f0b539cd3adcc0c0d6c607e9b20
JayHuLBL <jianjunhu@lbl.gov>
2024-04-12 13:30:39 -0700

    make list of the csv reader object

--- pyfunnel/pyfunnel.py
+++ pyfunnel/pyfunnel.py
@@ -88,8 +88,8 @@ if __name__ == "__main__":
     for s in ('reference', 'test'):
         data[s] = dict(x=[], y=[])
         with open(vars(args)[s]) as csvfile:
-            spamreader = csv.reader(csvfile)
-            if (len(next(spamreader)) != 2):
+            spamreader = list(csv.reader(csvfile))
+            if (len(spamreader[0]) != 2):
                 raise IOError("The {} CSV file must have exactly two columns.".format(s))
             for row in spamreader:
                 try:
